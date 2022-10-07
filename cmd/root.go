9f4189bea62605362ed1ebf5b16c9c5c05822e10
Evans Owamoyo <lordvidex.prime@gmail.com>
2022-10-07 21:46:12 +0300

    version bumped to 0.2.1, fixed versioning

--- cmd/root.go
+++ cmd/root.go
@@ -23,7 +23,7 @@ and count the number of characters in all files of a directory.
 
 It also includes various flags that can be used to customize the output of the tool.
 Run 'gomeasure --help' to see the available flags.`,
-		Version: "0.2",
+		Version: "0.2.1",
 	}
 	workersCount int
 	isVerbose    bool
