1b2a3cabeae46691fee82fe032c78a4aba0d9e33
escalopa <ahmad.helaly.dev@gmail.com>
2022-10-07 16:01:46 +0300

    refactor update command

--- cmd/update.go
+++ cmd/update.go
@@ -1,26 +1,9 @@
 package cmd
 
 import (
-	"bufio"
-	"encoding/json"
-	"fmt"
+	"github.com/lordvidex/gomeasure/pkg"
 	"github.com/spf13/cobra"
-	"net/http"
-	"os"
-	"os/exec"
-	"strings"
 )
-
-type Message struct {
-	Name string `json:"name"`
-	//ZipballUrl string `json:"zipball_url"`
-	//TarballUrl string `json:"tarball_url"`
-	//Commit     struct {
-	//	Sha string `json:"sha"`
-	//	Url string `json:"url"`
-	//} `json:"commit"`
-	//NodeId string `json:"node_id"`
-}
 
 // updateCmd represents the update command
 var updateCmd = &cobra.Command{
@@ -34,60 +17,12 @@ var updateCmd = &cobra.Command{
 }
 
 func update() error {
-	latest, err := getVersions()
-	if err != nil {
-		return err
-	}
-	if "v"+rootCmd.Version == latest {
-		fmt.Println("gomeasure is up to date")
-	} else {
-		fmt.Printf("Version %s is avaliable, install new verison?(y/n)", latest)
-		reader := bufio.NewReader(os.Stdin)
-		for true {
-			input, _ := reader.ReadString('\n')
-			input = strings.ToLower(input[:len(input)-1])
-			if input == "y" {
-				err = updateCli()
-				if err != nil {
-					return err
-				}
-				break
-			} else if input == "n" {
-				fmt.Println("Aborted update process")
-				break
-			} else {
-				fmt.Println("Unknown input please try again")
-			}
-		}
-	}
-	return nil
-}
-
-func updateCli() error {
-	branch := "apt-deploy"
-	link := "curl \"https://raw.githubusercontent.com/lordvidex/gomeasure/" + branch + "/scripts/install.sh\" | sh"
-	cmd := exec.Command("bash", "-c", link)
-	_, err := cmd.Output()
+	runner := &pkg.Runner{}
+	err := runner.Update(rootCmd.Version)
 	if err != nil {
 		return err
 	}
 	return nil
-}
-
-func getVersions() (string, error) {
-	url := "https://api.github.com/repos/lordvidex/gomeasure/tags"
-	resp, err := http.Get(url)
-	if err != nil {
-		return "", fmt.Errorf("cannot fetch URL %q: %v", url, err)
-	}
-	defer resp.Body.Close()
-
-	var messages []Message
-
-	err = json.NewDecoder(resp.Body).Decode(&messages)
-	latest := messages[len(messages)-1].Name
-
-	return latest, nil
 }
 
 func init() {
--- pkg/runner.go
+++ pkg/runner.go
@@ -2,10 +2,14 @@ package pkg
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
 	"github.com/gobwas/glob"
 	"github.com/pkg/errors"
 	"io/fs"
+	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -142,6 +146,75 @@ func (r *Runner) process(arr []*Result, wg *sync.WaitGroup) error {
 		result.LinesCount = lines
 	}
 	return nil
+}
+
+func (r *Runner) Update(currentVersion string) error {
+	latest, err := r.getVersions()
+	if err != nil {
+		return err
+	}
+
+	if "v"+currentVersion == latest {
+		fmt.Println("gomeasure is up to date")
+	} else {
+		fmt.Printf("Version %s is avaliable, install new verison?(y/n)", latest)
+		reader := bufio.NewReader(os.Stdin)
+		for true {
+			input, _ := reader.ReadString('\n')
+			input = strings.ToLower(input[:len(input)-1])
+			if input == "y" {
+				err = r.updateCli()
+				if err != nil {
+					return err
+				}
+				break
+			} else if input == "n" {
+				fmt.Println("Aborted update process")
+				break
+			} else {
+				fmt.Println("Unknown input please try again")
+			}
+		}
+	}
+	return nil
+}
+
+func (r *Runner) updateCli() error {
+	branch := "apt-deploy"
+	link := "curl \"https://raw.githubusercontent.com/lordvidex/gomeasure/" + branch + "/scripts/install.sh\" | sh"
+	cmd := exec.Command("bash", "-c", link)
+	_, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Runner) getVersions() (string, error) {
+	type Message struct {
+		Name string `json:"name"`
+		//ZipballUrl string `json:"zipball_url"`
+		//TarballUrl string `json:"tarball_url"`
+		//Commit     struct {
+		//	Sha string `json:"sha"`
+		//	Url string `json:"url"`
+		//} `json:"commit"`
+		//NodeId string `json:"node_id"`
+	}
+
+	url := "https://api.github.com/repos/lordvidex/gomeasure/tags"
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("cannot fetch URL %q: %v", url, err)
+	}
+	defer resp.Body.Close()
+
+	var messages []Message
+
+	err = json.NewDecoder(resp.Body).Decode(&messages)
+	latest := messages[len(messages)-1].Name
+
+	return latest, nil
 }
 
 func concatErrors(errs []error) error {
